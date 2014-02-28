1b30a5993bb888caa240b36f20db5d3089fd082e
Nitansh <nitansh.bareja@gmail.com>
2014-02-28 12:12:19 +0530

    Revert "commiting lint free base app in develop ,mode"
    This reverts commit ee4741329302ef9f68ae2aad5076fcf9505d7bfd.

--- snippets/static/Javascript/apps/BaseApp.js
+++ snippets/static/Javascript/apps/BaseApp.js
@@ -1,55 +1,56 @@
-define(function (
-    require
-) {
+define(function(
+			require
+		){
+	
+			"use strict";
 
-    "use strict";
+			function BaseApp (){
+					
+				var BaseRouter = require('router/baseRouter');
+				var PubSub     = require('libs/pubSub');
+				var self  =  this;
+				this.baseRouter =  new BaseRouter();
+				PubSub.listenTo(self.baseRouter, 'route:init', self.StateLogin);
+				PubSub.listenTo(self.baseRouter, 'route:userLoggedIn', self.UserAuthorized);
+				PubSub.on('render:ActiveView',self.renderActiveView);					
+			};
 
-    var BaseApp = function BaseAppF() {
-
-        var BaseRouter = require('router/baseRouter');
-        var PubSub = require('libs/pubSub');
-        var self = this;
-        this.baseRouter = new BaseRouter();
-        PubSub.listenTo(self.baseRouter, 'route:init', self.StateLogin);
-        PubSub.listenTo(self.baseRouter, 'route:userLoggedIn', self.UserAuthorized);
-        PubSub.on('render:ActiveView', self.renderActiveView);
-    };
-
-    BaseApp.prototype.UserAuthorized = function () {
-        var _this = this;
+			BaseApp.prototype.UserAuthorized= function(){
+				var _this = this;
 
 
-        require(['views/customer-view', 'libs/pubSub', 'views/header-view-userAuthorized', 'apps/UserAuthorizedApp'], function (CustomerView, PubSub, HeaderUserAuthorized, UserAuthorizedApp) {
-            PubSub.trigger('remove:bodyView', 'old body view deleted');
-            PubSub.trigger('remove:generalInformationView', 'old general information view deleted');
-            var customerView = new CustomerView();
-            window.activeView = customerView;
-            if (_this.headerUserAuthorized === undefined) {
-                PubSub.trigger('remove:headerView', 'old header view deleted');
-                _this.headerUserAuthorized = new HeaderUserAuthorized();
-                new UserAuthorizedApp();
-            }
+				require(['views/customer-view','libs/pubSub','views/header-view-userAuthorized','apps/UserAuthorizedApp'], function(CustomerView, PubSub, HeaderUserAuthorized, UserAuthorizedApp){
+					PubSub.trigger('remove:bodyView','old body view deleted'); 
+					PubSub.trigger('remove:generalInformationView','old general information view deleted');
+					var customerView = new CustomerView();
+					window.activeView = customerView;
+					if (_this.headerUserAuthorized === undefined){
+						PubSub.trigger('remove:headerView','old header view deleted'); 
+						_this.headerUserAuthorized =  new HeaderUserAuthorized();
+						var userAuthorizedApp = new UserAuthorizedApp();
+               		}
+               		
+				});
+			};
 
-        });
-    };
+			BaseApp.prototype.StateLogin =  function(msg){
+				
+				require(['views/header-view','libs/pubSub','views/footer-view','views/body-view'], function(HeaderView, PubSub, FooterView, BodyView){
+					PubSub.trigger('remove:customerView','customer body view deleted'); 
+					PubSub.trigger('remove:headerViewUserAuthorized','old header view deleted'); 
+					PubSub.trigger('remove:generalInformationView','old general information view deleted');
+					var headerView   = new HeaderView();
+					var footerView   = new FooterView();
+					var bodyView     = new BodyView();
+					window.activeView = bodyView;
+				});
+				this.headerUserAuthorized = undefined;
 
-    BaseApp.prototype.StateLogin = function () {
+			};
 
-        require(['views/header-view', 'libs/pubSub', 'views/footer-view', 'views/body-view'], function (HeaderView, PubSub, FooterView, BodyView) {
-            PubSub.trigger('remove:customerView', 'customer body view deleted');
-            PubSub.trigger('remove:headerViewUserAuthorized', 'old header view deleted');
-            PubSub.trigger('remove:generalInformationView', 'old general information view deleted');
-            new HeaderView();
-            new FooterView();
-            window.activeView = new BodyView();
-        });
-        this.headerUserAuthorized = undefined;
+			BaseApp.prototype.renderActiveView = function(){
+				window.activeView.render();
+			};
 
-    };
-
-    BaseApp.prototype.renderActiveView = function () {
-        window.activeView.render();
-    };
-
-    return BaseApp;
+			return BaseApp;
 });
--- snippets/static/Javascript/apps/UserAuthorizedApp.js
+++ snippets/static/Javascript/apps/UserAuthorizedApp.js
@@ -1,36 +1,38 @@
-define(function (
-    require
-) {
+define(function(
+			require
+		){
 
-    "use strict";
+		"use strict";
 
-    var userAuthorizedApp  = function userAuthorizedAppF() {
-        var UserAuthorizedRouter = require('router/UserAuthorizedRouter');
-        var PubSub = require('libs/pubSub');
-        var self = this;
-        this.userAuthorizedRouter = new UserAuthorizedRouter();
-        PubSub.listenTo(self.userAuthorizedRouter, 'route:generalinfo', self.stateGeneralInfo);
-        PubSub.listenTo(self.userAuthorizedRouter, 'route:dashboard', self.stateDashBoard);
-    };
+		function userAuthorizedApp(){
+			var UserAuthorizedRouter = require('router/UserAuthorizedRouter');
+			var PubSub               = require('libs/pubSub');
+			var self  =  this;
+			this.userAuthorizedRouter =  new UserAuthorizedRouter();
+			PubSub.listenTo(self.userAuthorizedRouter, 'route:generalinfo', self.stateGeneralInfo);
+			PubSub.listenTo(self.userAuthorizedRouter, 'route:dashboard', self.stateDashBoard);
+		}
 
-    userAuthorizedApp.prototype.stateGeneralInfo = function () {
-        require(['views/generalInformation-view', 'libs/pubSub'], function (GeneralInformationView, PubSub) {
-            PubSub.trigger('remove:bodyView', 'old body view deleted');
-            PubSub.trigger('remove:customerView', 'old customer information view deleted');
-            window.activeView = new GeneralInformationView();
-        });
+		userAuthorizedApp.prototype.stateGeneralInfo =  function(){
+			require(['views/generalInformation-view','libs/pubSub'], function(GeneralInformationView, PubSub){
+					PubSub.trigger('remove:bodyView','old body view deleted');  
+					PubSub.trigger('remove:customerView','old customer information view deleted'); 
+					var generalInformationView = new GeneralInformationView();	
+					window.activeView = generalInformationView;				
+			});            
 
-    };
+		}
 
-    userAuthorizedApp.prototype.stateDashBoard = function () {
-        require(['views/Dashboard-view', 'libs/pubSub'], function (DashboardView, PubSub) {
-            PubSub.trigger('remove:bodyView', 'old body view deleted');
-            PubSub.trigger('remove:customerView', 'old customer information view deleted');
-            PubSub.trigger('remove:generalInformationView', 'old customer information view deleted');
-            window.activeView = new DashboardView();
-        });
-    };
+		userAuthorizedApp.prototype.stateDashBoard =  function(){
+			require(['views/Dashboard-view','libs/pubSub'], function(DashboardView, PubSub){	
+				PubSub.trigger('remove:bodyView','old body view deleted');  
+				PubSub.trigger('remove:customerView','old customer information view deleted');
+				PubSub.trigger('remove:generalInformationView','old customer information view deleted');			 
+            	var dashboardView = new DashboardView();
+            	window.activeView = dashboardView;
+			})
+		}
 
-    return userAuthorizedApp;
+		return userAuthorizedApp;
 
 });
