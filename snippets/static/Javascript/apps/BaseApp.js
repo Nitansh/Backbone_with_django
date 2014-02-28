ee4741329302ef9f68ae2aad5076fcf9505d7bfd
unknown <gur33247@GURWC6023.ad.aricent.com>
2014-02-28 12:10:54 +0530

    commiting lint free base app in develop ,mode

--- snippets/static/Javascript/apps/BaseApp.js
+++ snippets/static/Javascript/apps/BaseApp.js
@@ -1,56 +1,55 @@
-define(function(
-			require
-		){
-	
-			"use strict";
+define(function (
+    require
+) {
 
-			function BaseApp (){
-					
-				var BaseRouter = require('router/baseRouter');
-				var PubSub     = require('libs/pubSub');
-				var self  =  this;
-				this.baseRouter =  new BaseRouter();
-				PubSub.listenTo(self.baseRouter, 'route:init', self.StateLogin);
-				PubSub.listenTo(self.baseRouter, 'route:userLoggedIn', self.UserAuthorized);
-				PubSub.on('render:ActiveView',self.renderActiveView);					
-			};
+    "use strict";
 
-			BaseApp.prototype.UserAuthorized= function(){
-				var _this = this;
+    var BaseApp = function BaseAppF() {
+
+        var BaseRouter = require('router/baseRouter');
+        var PubSub = require('libs/pubSub');
+        var self = this;
+        this.baseRouter = new BaseRouter();
+        PubSub.listenTo(self.baseRouter, 'route:init', self.StateLogin);
+        PubSub.listenTo(self.baseRouter, 'route:userLoggedIn', self.UserAuthorized);
+        PubSub.on('render:ActiveView', self.renderActiveView);
+    };
+
+    BaseApp.prototype.UserAuthorized = function () {
+        var _this = this;
 
 
-				require(['views/customer-view','libs/pubSub','views/header-view-userAuthorized','apps/UserAuthorizedApp'], function(CustomerView, PubSub, HeaderUserAuthorized, UserAuthorizedApp){
-					PubSub.trigger('remove:bodyView','old body view deleted'); 
-					PubSub.trigger('remove:generalInformationView','old general information view deleted');
-					var customerView = new CustomerView();
-					window.activeView = customerView;
-					if (_this.headerUserAuthorized === undefined){
-						PubSub.trigger('remove:headerView','old header view deleted'); 
-						_this.headerUserAuthorized =  new HeaderUserAuthorized();
-						var userAuthorizedApp = new UserAuthorizedApp();
-               		}
-               		
-				});
-			};
+        require(['views/customer-view', 'libs/pubSub', 'views/header-view-userAuthorized', 'apps/UserAuthorizedApp'], function (CustomerView, PubSub, HeaderUserAuthorized, UserAuthorizedApp) {
+            PubSub.trigger('remove:bodyView', 'old body view deleted');
+            PubSub.trigger('remove:generalInformationView', 'old general information view deleted');
+            var customerView = new CustomerView();
+            window.activeView = customerView;
+            if (_this.headerUserAuthorized === undefined) {
+                PubSub.trigger('remove:headerView', 'old header view deleted');
+                _this.headerUserAuthorized = new HeaderUserAuthorized();
+                new UserAuthorizedApp();
+            }
 
-			BaseApp.prototype.StateLogin =  function(msg){
-				
-				require(['views/header-view','libs/pubSub','views/footer-view','views/body-view'], function(HeaderView, PubSub, FooterView, BodyView){
-					PubSub.trigger('remove:customerView','customer body view deleted'); 
-					PubSub.trigger('remove:headerViewUserAuthorized','old header view deleted'); 
-					PubSub.trigger('remove:generalInformationView','old general information view deleted');
-					var headerView   = new HeaderView();
-					var footerView   = new FooterView();
-					var bodyView     = new BodyView();
-					window.activeView = bodyView;
-				});
-				this.headerUserAuthorized = undefined;
+        });
+    };
 
-			};
+    BaseApp.prototype.StateLogin = function () {
 
-			BaseApp.prototype.renderActiveView = function(){
-				window.activeView.render();
-			};
+        require(['views/header-view', 'libs/pubSub', 'views/footer-view', 'views/body-view'], function (HeaderView, PubSub, FooterView, BodyView) {
+            PubSub.trigger('remove:customerView', 'customer body view deleted');
+            PubSub.trigger('remove:headerViewUserAuthorized', 'old header view deleted');
+            PubSub.trigger('remove:generalInformationView', 'old general information view deleted');
+            new HeaderView();
+            new FooterView();
+            window.activeView = new BodyView();
+        });
+        this.headerUserAuthorized = undefined;
 
-			return BaseApp;
+    };
+
+    BaseApp.prototype.renderActiveView = function () {
+        window.activeView.render();
+    };
+
+    return BaseApp;
 });
